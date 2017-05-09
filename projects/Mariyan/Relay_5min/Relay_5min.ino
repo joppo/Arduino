83a55a1c474cef76b8d0cf2d93754bc9878b0765
joppo <zhivkogospodinov@gmail.com>
2017-05-09 15:01:51 +0300

    added constants and variables for the timing

--- projects/Mariyan/Relay_5min/Relay_5min.ino
+++ projects/Mariyan/Relay_5min/Relay_5min.ino
@@ -1,16 +1,31 @@
 #define CH1 4   // Connect Digital Pin 4 on Arduino to CH1 on Relay Module
 
+uint signalLength = 2000;
+uint pauseBetweenSignals = 2000;
+uint loop_period = 300000; //5 minutes
+
  void setup(){
-   //Setup all the Arduino Pins
+   
    pinMode(CH1, OUTPUT);
    digitalWrite(CH1,LOW);
 
-   delay(2000); //Wait 2 seconds before starting sequence
+   delay(loop_period); //we wait for 5 minutes before signaling with the siren
  }
 
  void loop(){
    digitalWrite(CH1, HIGH);
-   delay(1000);
+   delay(signalLength);
    digitalWrite(CH1, LOW);
-   delay(1000);
+   delay(pauseBetweenSignals);
+   
+   digitalWrite(CH1, HIGH);
+   delay(signalLength);
+   digitalWrite(CH1, LOW);
+   delay(pauseBetweenSignals);
+   
+   digitalWrite(CH1, HIGH);
+   delay(signalLength);
+   digitalWrite(CH1, LOW);
+   
+   delay(loop_period - signalLength*3 - pauseBetweenSignals*2); //this is 5 minutes minus 10 seconds (the signaling takes 10 seconds to end)
  }
